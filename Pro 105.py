19ef5991edc5ffd8ddf8873f48bff94c0fbdafd7
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2022-11-20 20:46:41 +0530

    Add files via upload

---
+++ Pro 105.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+path = "/Users/cs/Documents/WhiteHat/Python/Images"
+Images=[]
+for file in os.listdir(path):
+    name,ext = os.path.splitext(file)
+
+    if ext in ['.gif','.png','.jpg','.jpeg','.jfif']:
+        file_name = path +"/"+file
+
+        print(file_name)
+
+        Images.append(file_name)
+
+print(len(Images))
+count= len(Images)
+frame = cv2.imread(Images[0])
+height,width,channels = frame.shape
+size = (width,height)
+print(size)
+out = cv2.VideoWriter("Freinds.mp4",cv2.VideoWriter_fourcc(*'DIVX'),0.5,size)
+for i in range (0,count):
+    print(Images[i])
+    frame = cv2.imread(Images[i])
+    out.write(frame)
+
+out.release()
+print("done")
+
+
